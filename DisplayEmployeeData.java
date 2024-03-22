b2a7b3a91f8e774fe271143765228ba098844c37
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-03-22 11:11:16 +0530

    DisplayEmployeeData.java

---
+++ DisplayEmployeeData.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class DisplayEmployeeData {
+public static void main(String[] args) {
+
+try {
+Class.forName(&quot;com.mysql.jdbc.Driver&quot;);
+
+Connection conn =
+DriverManager.getConnection(&quot;jdbc:mysql://localhost:3306/emp_db?characterEncoding=ut
+f8&quot;,
+&quot;root&quot;,
+&quot;&quot;);
+
+CallableStatement cstmt = conn.prepareCall(&quot;{CALL GetAllEmployees()}&quot;);
+
+ResultSet rs = cstmt.executeQuery();
+
+Department of Computer Science, Rajagiri College of Science 115 Page
+System.out.println(&quot;Employee Data:&quot;);
+System.out.println(&quot;Ename\tSalary&quot;);
+while (rs.next()) {
+String ename = rs.getString(&quot;Ename&quot;);
+double salary = rs.getDouble(&quot;esal&quot;);
+System.out.println(ename + &quot;\t&quot; + salary);
+}
+
+// Close resources
+rs.close();
+cstmt.close();
+conn.close();
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+}
